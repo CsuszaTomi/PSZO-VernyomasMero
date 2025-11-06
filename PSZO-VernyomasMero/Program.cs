474a6876a965f9a717d01fa2d2c756cac0acf087
HunTrooper <dgamer1118offical2@gmail.com>
2025-11-06 15:22:42 +0100

    Usingok berakása

--- PSZO-VernyomasMero/Program.cs
+++ PSZO-VernyomasMero/Program.cs
@@ -1,6 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 using System;
+using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MyApp
 {
